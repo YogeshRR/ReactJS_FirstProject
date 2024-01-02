e8a402860cd885dba6c205c6e53220580b7c3829
YogeshRR <43900564+YogeshRR@users.noreply.github.com>
2024-01-02 15:40:23 +0530

    dynamicaly adds list of items

--- src/App.jsx
+++ src/App.jsx
@@ -31,20 +31,9 @@ function App() {
         <section id='core-concepts'>
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcepts
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcepts
-              {...CORE_CONCEPTS[1]}
-            />
-            <CoreConcepts
-              {...CORE_CONCEPTS[2]}
-            />
-            <CoreConcepts
-              {...CORE_CONCEPTS[3]}
-            />
+
+            {CORE_CONCEPTS.map((conceptItem) => (<CoreConcepts key={conceptItem.title} {...conceptItem} />))}
+
           </ul>
         </section>
         <section id='examples'>
